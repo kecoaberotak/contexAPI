ef37b08a55a1307de3af3fb852f203c5b3b030a9
KecoaBerotak <dennisfernandes0212@gmail.com>
2022-02-04 16:30:40 +0700

    mengubah context class component, menjadi useContext menggunaka functional component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,71 @@
-import React from "react";
+import React, { useContext } from "react";
 
 // didalam component yang dipanggil adalah contextType
 import { ThemeContext } from "../context/ThemeContext";
 import { AuthContext } from "./AuthContext";
 
-class Navbar extends React.Component {
-    // mengaktifkan context di componen
-    static contextType = ThemeContext;
-    render() {    
-        return (
-            // bisa memiliki lebih dari satu context, tampung context lainnya didalam context yang lain
-            <AuthContext.Consumer>{(authContext) => {
-                return(
-                    // menggunakan context consumer, memberikan hasil yang sama
-                    // context consumer bisa digunakan untuk funsional component
-                    <ThemeContext.Consumer>{(themeContext) => {
-                        // mengambil nilai context    
-                        const {isDarkTheme, darkTheme, lightTheme} = themeContext;
-                        const {isLoggedIn, changeAuthStatus} = authContext;
-                        const theme = isDarkTheme ? darkTheme : lightTheme;
-                        return (
-                            <nav style={{background: theme.background, color: theme.text, height: '120px'}}>
-                                <h2 style={{textAlign: 'center'}} >
-                                    Yo Academy
-                                </h2>
-                                <p onClick={changeAuthStatus} style={{textAlign: 'center'}}>{ isLoggedIn ? 'logged in' : 'logged out' }</p>
-                                <div className="ui three buttons">
-                                    <button className="ui button">Overview</button>
-                                    <button className="ui button">Contact</button>
-                                    <button className="ui button">Support</button>
-                                </div>
-                            </nav>                    
-                        )
-                    }}</ThemeContext.Consumer>                    
-                )
-            }}</AuthContext.Consumer>
-        )
-    }
+
+
+// Multiple Context menggunakan Functional Component
+const Navbar = () => {
+    const {isLoggedIn, changeAuthStatus} = useContext(AuthContext);
+    const {isDarkTheme, darkTheme, lightTheme} = useContext(ThemeContext);
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+
+    return (
+        <nav style={{background: theme.background, color: theme.text, height: '120px'}}>
+            <h2 style={{textAlign: 'center'}} >
+                Yo Academy
+            </h2>
+            <p onClick={changeAuthStatus} style={{textAlign: 'center'}}>{ isLoggedIn ? 'logged in' : 'logged out' }</p>
+            <div className="ui three buttons">
+                <button className="ui button">Overview</button>
+                <button className="ui button">Contact</button>
+                <button className="ui button">Support</button>
+            </div>
+        </nav>
+    );
 }
+
+
+
+
+
+
+// Multiple Context menggunakan Class Component
+// class Navbar extends React.Component {
+//     // mengaktifkan context di componen
+//     static contextType = ThemeContext;
+//     render() {    
+//         return (
+//             // bisa memiliki lebih dari satu context, tampung context lainnya didalam context yang lain
+//             <AuthContext.Consumer>{(authContext) => {
+//                 return(
+//                     // menggunakan context consumer, memberikan hasil yang sama
+//                     // context consumer bisa digunakan untuk funsional component
+//                     <ThemeContext.Consumer>{(themeContext) => {
+//                         // mengambil nilai context    
+//                         const {isDarkTheme, darkTheme, lightTheme} = themeContext;
+//                         const {isLoggedIn, changeAuthStatus} = authContext;
+//                         const theme = isDarkTheme ? darkTheme : lightTheme;
+//                         return (
+                            // <nav style={{background: theme.background, color: theme.text, height: '120px'}}>
+                            //     <h2 style={{textAlign: 'center'}} >
+                            //         Yo Academy
+                            //     </h2>
+                            //     <p onClick={changeAuthStatus} style={{textAlign: 'center'}}>{ isLoggedIn ? 'logged in' : 'logged out' }</p>
+                            //     <div className="ui three buttons">
+                            //         <button className="ui button">Overview</button>
+                            //         <button className="ui button">Contact</button>
+                            //         <button className="ui button">Support</button>
+                            //     </div>
+                            // </nav>                    
+//                         )
+//                     }}</ThemeContext.Consumer>                    
+//                 )
+//             }}</AuthContext.Consumer>
+//         )
+//     }
+// };
 
 export default Navbar;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,27 +1,59 @@
-import React from "react";
+import React, {useContext} from "react";
 
 // didalam component yang dipanggil adalah contextType
 import { ThemeContext } from "../context/ThemeContext";
 
-class TodoList extends React.Component {
-    // mengaktifkan context di componen
-    static contextType = ThemeContext;
-    render() {
-        // mengambil nilai context
-        const {isDarkTheme, lightTheme, darkTheme, changeTheme} = this.context;
-        const theme = isDarkTheme ? darkTheme : lightTheme;
-        return (
-            <div style={{background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}}>
-                <p className="item">Plan the family trip</p>
-                <p className="item">Go for shopping for dinner</p>
-                <p className="item">Go for a walk</p>
 
-                {/* tombol untuk mengubah theme */}
-                {/* walau tombolnya hanya ada di componen ini tapi nilai context di component lain juga berubah */}
-                <button className="ui button primary" onClick={changeTheme}>Change the theme</button>
-            </div>
-        )
-    }
+// MENGGUNAKAN FUNCTIONAL COMPONENT
+
+const TodoList = () => {
+    // memanggil context yg mau dipakai
+    const {isDarkTheme, lightTheme, darkTheme, changeTheme} = useContext(ThemeContext);
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+
+    return (
+        <div style={{background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}}>
+            <p className="item">Plan the family trip</p>
+            <p className="item">Go for shopping for dinner</p>
+            <p className="item">Go for a walk</p>
+
+            {/* tombol untuk mengubah theme */}
+            {/* walau tombolnya hanya ada di componen ini tapi nilai context di component lain juga berubah */}
+            <button className="ui button primary" onClick={changeTheme}>Change the theme</button>
+        </div>        
+    )
 }
+
+
+
+
+
+
+
+
+
+
+// MENGGUNAKAN CLASS COMPONENT  
+
+// class TodoList extends React.Component {
+//     // mengaktifkan context di componen
+//     static contextType = ThemeContext;
+//     render() {
+//         // mengambil nilai context
+//         const {isDarkTheme, lightTheme, darkTheme, changeTheme} = this.context;
+//         const theme = isDarkTheme ? darkTheme : lightTheme;
+//         return (
+            // <div style={{background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}}>
+            //     <p className="item">Plan the family trip</p>
+            //     <p className="item">Go for shopping for dinner</p>
+            //     <p className="item">Go for a walk</p>
+
+            //     {/* tombol untuk mengubah theme */}
+            //     {/* walau tombolnya hanya ada di componen ini tapi nilai context di component lain juga berubah */}
+            //     <button className="ui button primary" onClick={changeTheme}>Change the theme</button>
+            // </div>
+//         )
+//     }
+// };
 
 export default TodoList;
