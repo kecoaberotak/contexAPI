6635ea7c47a557253eec14ad2fcd7beebcd3ade6
KecoaBerotak <dennisfernandes0212@gmail.com>
2022-02-07 09:37:42 +0700

    fungsi tambah data ke state

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 
 // didalam component yang dipanggil adalah contextType
 import { ThemeContext } from "../context/ThemeContext";
@@ -8,23 +8,46 @@ import { TodoListContext } from "../context/TodoListContext";
 // MENGGUNAKAN FUNCTIONAL COMPONENT
 
 const TodoList = () => {
+    const [todo, setTodo] = useState('');
     // memanggil context yg mau dipakai
-    const {todos} = useContext(TodoListContext);
+    const {todos, addTodo} = useContext(TodoListContext);
     const {isDarkTheme, lightTheme, darkTheme, changeTheme} = useContext(ThemeContext);
     const theme = isDarkTheme ? darkTheme : lightTheme;
 
-    return (
-        <div style={{background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}}>
-            <p className="item">Plan the family trip</p>
-            <p className="item">Go for shopping for dinner</p>
-            <p className="item">Go for a walk</p>
 
+    // fungsi menampung data ke state sementara (todo)
+    const handleChange = (e) => {
+        setTodo(e.target.value);
+    };
+
+    // menambahkan data ke state utama (todos)
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        addTodo(todo);
+    }
+
+    return (
+        <div style={{background: theme.background, color: theme.text, textAlign: 'center'}}>
+            {/* menampilkan data dari state */}
+            {todos.length ? (todos.map((todo) => {
+                    return <p className="item" key = {todo.id}>{todo.text}</p>
+                })) 
+                : (
+                    <div>You have no todos</div>
+                )
+            }
+            {/* menambahkan data baru ke state */}
+            <form onSubmit={handleFormSubmit}>
+                <label htmlFor="todo">Add Todo</label>
+                <input type="text" id="todo" onChange={handleChange}/>
+                <input type="submit" value="Add new todo" className="ui button primary"/>
+            </form>
             {/* tombol untuk mengubah theme */}
             {/* walau tombolnya hanya ada di componen ini tapi nilai context di component lain juga berubah */}
             <button className="ui button primary" onClick={changeTheme}>Change the theme</button>
-        </div>        
-    )
-}
+        </div>
+    );
+};
 
 
 
