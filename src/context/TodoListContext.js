7502ad90db52c77a9fffa516fed4fd745a346b96
KecoaBerotak <dennisfernandes0212@gmail.com>
2022-02-07 10:40:01 +0700

    menyimpan reducer kedalam file yang terpisah

--- src/context/TodoListContext.js
+++ src/context/TodoListContext.js
@@ -1,21 +1,7 @@
 import React, {createContext, useReducer} from "react";
+import { todosReducer } from "../reducers/TodosReduce";
 
 export const TodoListContext = createContext();
-
-// menggunakan reducer bukan state
-const todosReducer = (state, action) => {
-    switch(action.type) {
-        case 'ADD_TODO': 
-            return [
-                ...state,
-                { text: action.text, id: Math.random() }
-            ];
-        case 'REMOVE_TODO':
-            return state.filter((todo) => todo.id !== Number(action.id));
-        default:
-            return state;
-    }
-};
 
 const TodoListContextProvider = ({children}) => {
     // memanggil reducer
@@ -24,6 +10,16 @@ const TodoListContextProvider = ({children}) => {
         {text: 'Go shopping for dinner', id: 2},
         {text: 'Go for a walk', id: 3}
     ]);
+
+
+    return(
+        <TodoListContext.Provider value={{todos, dispatch}}>
+            {children}
+        </TodoListContext.Provider>
+    )
+};
+
+export default TodoListContextProvider;
 
 
     // // ini pakai state
@@ -41,12 +37,3 @@ const TodoListContextProvider = ({children}) => {
     //         return todo.id !== Number(id);
     //     }));
     // };
-
-    return(
-        <TodoListContext.Provider value={{todos, dispatch}}>
-            {children}
-        </TodoListContext.Provider>
-    )
-};
-
-export default TodoListContextProvider;
